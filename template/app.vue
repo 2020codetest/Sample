29492c242a1ec8f79c6a33740a8493f3a7b3a30c
David Zeng <davzen@microsoft.com>
2020-04-25 21:03:37 +0800

    update decorator

--- template/app.vue
+++ template/app.vue
@@ -6,6 +6,8 @@
 </template>
 <script lang="ts">
     import Vue from "vue"
+    import {Component} from "vue-property-decorator"
+    @Component({name: "App"})
     export default class App extends Vue{
         msg: string = "hello world"
         src: string = require("./test.jpg")
