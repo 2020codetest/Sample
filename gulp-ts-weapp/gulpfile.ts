0f0eeb7625699327aefcf5a6d5532871902cab39
David Zeng <davzen@microsoft.com>
2020-06-05 19:13:34 +0800

    add sass task

--- gulp-ts-weapp/gulpfile.ts
+++ gulp-ts-weapp/gulpfile.ts
@@ -1,8 +1,10 @@
 import GulpClient from "gulp"
 const tsPlugin = require("gulp-typescript")
 const plumber = require('gulp-plumber');
-let tsProject = tsPlugin.createProject("tsconfig.json");
+const sass = require("gulp-sass")
+const rename = require("gulp-rename")
 
+let tsProject = tsPlugin.createProject("tsconfig.json");
 function tsTask(){
     return tsProject.src()
     .pipe(plumber({
@@ -30,9 +32,25 @@ function debounce(task: () => void, delay: number): (done: (error?: any) => void
     }
 }
 
-function watchTask() {
-    GulpClient.watch(["./**/*.ts", "!node_modules/**"], tsTask)
+function sassTask() : any{
+    return GulpClient.src(["./**/*.sass", '!node_modules/**'])
+    .pipe(plumber({
+        errorHandler: function(error: any){
+            console.log(error.toString())
+            this.emit('end')
+        }
+    }))
+    .pipe(sass())
+    .pipe(rename({extname: ".wxss"}))
+    .pipe(GulpClient.dest(function (file: any) {
+        return file.base;
+    }))
 }
 
-GulpClient.task("build", tsTask)
+function watchTask() {
+    GulpClient.watch(["./**/*.ts", "!node_modules/**"], tsTask)
+    GulpClient.watch(["./**/*.sass", "!node_modules/**"], sassTask)
+}
+
+GulpClient.task("build", GulpClient.parallel([tsTask, sassTask]))
 GulpClient.task("watch", debounce(watchTask, 2500))
